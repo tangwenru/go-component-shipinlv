0aa9738452855f92ae17ba6e78fcb4baf932f0f3
tangwenru <i@alone.cat>
2024-10-07 15:02:20 +0800

    door 2

--- subsystem/video-publish/type/video-topic.go
+++ subsystem/video-publish/type/video-topic.go
@@ -1,11 +1,11 @@
 package typeVideoTopic
 
 type VideoTopicTopicList struct {
-	Id      int64   `json:"id"`
-	GroupId int64   `json:"groupId"`
-	Title   float64 `json:"title"`
-	Enabled bool    `json:"enabled"`
-	Created int64   `json:"created"`
+	Id      int64  `json:"id"`
+	GroupId int64  `json:"groupId"`
+	Title   string `json:"title"`
+	Enabled bool   `json:"enabled"`
+	Created int64  `json:"created"`
 }
 
 type VideoTopicTopicListQuery struct {
