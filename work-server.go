ad99ff8f5001c1fb0629c984ac209539cbcfcc44
tangwenru <i@alone.cat>
2025-04-01 01:08:47 +0800

    door 2

--- work-server.go
+++ work-server.go
@@ -10,39 +10,88 @@ import (
 type WorkServer struct {
 }
 
-type WorkServerDetail struct {
+type WorkServerRandDetail struct {
 	Id             int64  `json:"id"`
 	Title          string `json:"title"`
 	Domain         string `json:"domain"`
 	DomainProtocol string `json:"domainProtocol"`
 	Port           int64  `json:"port"`
 	WorkType       string `json:"workType"`
+	Enabled        bool   `json:"enabled"`
 }
 
-type WorkServerDetailQuery struct {
+type WorkServerRandDetailQuery struct {
 	WorkType string `json:"workType"`
 }
 
+type WorkServerRandDetailResult struct {
+	Success bool                 `json:"success"`
+	Message string               `json:"message"`
+	Data    WorkServerRandDetail `json:"data"`
+}
+
+// ----------------------
 type WorkServerDetailResult struct {
 	Success bool             `json:"success"`
 	Message string           `json:"message"`
 	Data    WorkServerDetail `json:"data"`
+}
+type WorkServerDetailQuery struct {
+	Id int64 `json:"id"`
+}
+
+type WorkServerDetail struct {
+	Id                       int64  `json:"id"`
+	Title                    string `json:"title"`
+	Domain                   string `json:"domain"`
+	DomainProtocol           string `json:"domainProtocol"`
+	Port                     int64  `json:"port"`
+	SshPort                  int64  `json:"sshPort"`
+	WorkType                 string `json:"workType"` // '','auto-video','data-agent') COLLATE utf8mb3_unicode_ci NOT NULL,
+	Enabled                  bool   `json:"enabled"`
+	SelfStart                bool   `json:"selfStart"`
+	Introduction             string `json:"introduction"`
+	LastGetAutoVideoTaskTime int64  `json:"lastGetAutoVideoTaskTime"`
+	ClientServeVersion       string `json:"clientServeVersion"`
+	LastHeart                int64  `json:"lastHeart"`
+	WorkStatus               string `json:"workStatus"`
+	Created                  int64  `json:"created"`
+	Updated                  int64  `json:"updated"`
 }
 
 func init() {
 
 }
 
-func (this *WorkServer) RandDetail(userId int64, workType string) (*WorkServerDetail, error) {
-	query := WorkServerDetailQuery{
+func (this *WorkServer) RandDetail(userId int64, workType string) (*WorkServerRandDetail, error) {
+	query := WorkServerRandDetailQuery{
 		WorkType: workType,
 	}
-	vipListResult := WorkServerDetailResult{}
+	vipListResult := WorkServerRandDetailResult{}
 
 	bytesResult, err := component_shipinlv_lib.MainSystem(userId, "workServer/randDetail", &query, &vipListResult)
 
 	if err != nil {
 		fmt.Println("Work Server RandDetail err:", string(bytesResult), err)
+	}
+
+	if !vipListResult.Success {
+		return nil, errors.New(vipListResult.Message)
+	}
+
+	return &vipListResult.Data, err
+}
+
+func (this *WorkServer) Detail(userId, id int64) (*WorkServerDetail, error) {
+	query := WorkServerDetailQuery{
+		Id: id,
+	}
+	vipListResult := WorkServerDetailResult{}
+
+	bytesResult, err := component_shipinlv_lib.MainSystem(userId, "workServer/detail", &query, &vipListResult)
+
+	if err != nil {
+		fmt.Println("Work Server Detail err:", string(bytesResult), err)
 	}
 
 	if !vipListResult.Success {
